1755f8f26d151c0ae807b4579a59a8d7518b9a47
Anastasia <anestezzzzzia@yandex.ru>
2024-10-31 20:36:50 +0300

    finish

--- test/testfunc.py
+++ test/testfunc.py
@@ -1,11 +1,5 @@
 def plus(a,b):
     return a + b
 
-txt = "new text"
-
-def minus(a,b):
-    return a + b
-
 if __name__ =="__main__":
-    print(plus(2, 3))
-    print(minus(2, 3))
+    print(plus(2,3))
